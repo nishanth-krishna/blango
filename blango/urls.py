014b9d3fc299d2483e44c47bf1f74b61c0fcb96e
C NISHANTHKRISHNA <cnishanthkrishna_cse1805d4@mgit.ac.in>
2022-09-05 05:04:44 +0000

    Finish DRF views

--- blango/urls.py
+++ blango/urls.py
@@ -40,7 +40,7 @@ urlpatterns = [
     path("accounts/", include("django_registration.backends.activation.urls")), #this is for two step authentication stuff
     path("accounts/", include("allauth.urls")),
 
-    path("api/v1/", include("blog.api_urls")), # for api urls
+    path("api/v1/", include("blog.api.urls")), # for api urls
 ]
 
 if settings.DEBUG:
